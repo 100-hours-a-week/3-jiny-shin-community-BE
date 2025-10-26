cca6204c06a1521a7f7cd2549587a97de0320b92
Jinyoung Shin <jinyshin.dev@gmail.com>
2025-10-26 20:34:38 +0900

    feat: 게시물/댓글/좋아요 Exception 메서드 추가

--- src/main/java/com/jinyshin/ktbcommunity/global/exception/ResourceNotFoundException.java
+++ src/main/java/com/jinyshin/ktbcommunity/global/exception/ResourceNotFoundException.java
@@ -9,4 +9,20 @@ public class ResourceNotFoundException extends ApiException {
     public static ResourceNotFoundException user() {
         return new ResourceNotFoundException(ApiErrorCode.USER_NOT_FOUND);
     }
+
+    public static ResourceNotFoundException post() {
+        return new ResourceNotFoundException(ApiErrorCode.POST_NOT_FOUND);
+    }
+
+    public static ResourceNotFoundException comment() {
+        return new ResourceNotFoundException(ApiErrorCode.COMMENT_NOT_FOUND);
+    }
+
+    public static ResourceNotFoundException image() {
+        return new ResourceNotFoundException(ApiErrorCode.IMAGE_NOT_FOUND);
+    }
+
+    public static ResourceNotFoundException like() {
+        return new ResourceNotFoundException(ApiErrorCode.LIKE_NOT_FOUND);
+    }
 }
