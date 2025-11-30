106578637965c64d2aea1387b5ad8dca52795ccd
Jinyoung Shin <jinyshin.dev@gmail.com>
2025-11-30 22:31:35 +0900

    fix: test profile activate

--- src/test/java/com/jinyshin/ktbcommunity/KtbCommunityApplicationTests.java
+++ src/test/java/com/jinyshin/ktbcommunity/KtbCommunityApplicationTests.java
@@ -2,12 +2,14 @@ package com.jinyshin.ktbcommunity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class KtbCommunityApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+  @Test
+  void contextLoads() {
+  }
 
 }
